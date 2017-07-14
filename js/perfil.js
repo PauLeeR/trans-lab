78b2386b646b2b3bd453e6c678b15febba66fb00
Paulina Rojas <lindaliel@gmail.com>
2017-07-14 12:19:52 -0400

    calcular tarifa estilos

--- js/perfil.js
+++ js/perfil.js
@@ -7,3 +7,18 @@ $(document).ready(function() {   //menu desplegable
                 $(sel2).toggleClass('out');
             });
         });
+
+$.ajax({
+            url : 'http://bip-servicio.herokuapp.com/api/v1/solicitudes.json',
+            type : 'GET',
+            datatype : 'json',
+            data : {'bip': valor}
+        })
+        .done(function(respuesta){
+            console.log("successe");
+            console.log(respuesta);
+            verSaldo(respuesta);
+        })
+        .fail(function(){
+            console.log("error");
+        })
